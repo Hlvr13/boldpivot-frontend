49071c71871027aa7ef0a9b2ecaa42b10b58b6d0
LeviVazquez <hlvr.13@gmail.com>
2020-06-10 01:17:20 -0700

    :wrench:[Redux-Headlines] Addes fav key to headline items and created setToFavorite method

--- src/redux/reducers/headlines.js
+++ src/redux/reducers/headlines.js
@@ -4,10 +4,11 @@ import Immutable from 'seamless-immutable'
 /* ------------- Types and Action Creators ------------- */
 
 const { Types, Creators } = createActions({
-    getAllHeadlines: ['Fields','PropKey'],
-    getEnHeadlines: ['Fields','PropKey'],
-    getCustomHeadlines: ['Fields', 'ARGS','PropKey'],
-    setHeadlineDataToProps: ['Data', 'PropKey'],
+  getAllHeadlines: ['Fields', 'PropKey'],
+  getEnHeadlines: ['Fields', 'PropKey'],
+  getCustomHeadlines: ['Fields', 'ARGS', 'PropKey'],
+  setToFavorite: ['Index', 'Id'],
+  setHeadlineDataToProps: ['Data', 'PropKey']
 })
 
 export const HeadlinesTypes = Types
@@ -16,10 +17,10 @@ export const HeadlinesActions = Creators
 /* ------------- Initial State ------------- */
 
 export const INITIAL_STATE = Immutable({
-    fetching: false,
-    allHeadlines: [],
-    enHeadlines: [],
-    customHeadlines: [],
+  fetching: false,
+  allHeadlines: [],
+  enHeadlines: [],
+  customHeadlines: []
 })
 
 /* ------------- Selectors ------------- */
@@ -27,18 +28,37 @@ export const INITIAL_STATE = Immutable({
 /* ------------- Reducers ------------- */
 
 export const setHeadlineDataToProps = (state, action) => {
-    const { Data, PropKey } = action
-    return state.merge({
-        [PropKey]: Data
-    })
+  const { Data, PropKey } = action
+  const updatedDate = PropKey.toLowerCase().includes('headline') ? [...Data].map(d => ({ ...d, fav: false })) : Data
+
+  return state.merge({
+    [PropKey]: updatedDate
+  })
 }
 
+export const setToFavorite = (state, action) => {
+  const { customHeadlines } = state
+  const { Index, Id } = action
+
+  const i = Id ? customHeadlines.findIndex(headline => headline.id === Id) : Index
+
+  const updatedHeadlines = [...customHeadlines]
+  updatedHeadlines[i] = {
+    ...customHeadlines[i],
+    fav: !customHeadlines[i].fav
+  }
+
+  return state.merge({
+    customHeadlines: updatedHeadlines
+  })
+}
 
 /* ------------- Hookup Reducers To Types ------------- */
 
 export const reducer = createReducer(INITIAL_STATE, {
-    [Types.GET_ALL_HEADLINES]: null,
-    [Types.GET_EN_HEADLINES]: null,
-    [Types.GET_CUSTOM_HEADLINES]: null,
-    [Types.SET_HEADLINE_DATA_TO_PROPS]: setHeadlineDataToProps,
+  [Types.GET_ALL_HEADLINES]: null,
+  [Types.GET_EN_HEADLINES]: null,
+  [Types.GET_CUSTOM_HEADLINES]: null,
+  [Types.SET_TO_FAVORITE]: setToFavorite,
+  [Types.SET_HEADLINE_DATA_TO_PROPS]: setHeadlineDataToProps
 })
