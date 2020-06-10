020d194532f42e7b5668c494e9f19866770f66be
LeviVazquez <hlvr.13@gmail.com>
2020-06-09 23:45:43 -0700

    :hammer:[Redux-Store] Created store with saga functionality

---
+++ src/redux/store.js
@@ -0,0 +1,13 @@
+import { createStore, applyMiddleware } from 'redux'
+import reducers from './reducers'
+
+import createSagaMiddleware from 'redux-saga'
+import sagaRoot from '../saga'
+
+const sagaMiddleware = createSagaMiddleware()
+
+
+
+const store = createStore(reducers, applyMiddleware(sagaMiddleware))
+export default store
+sagaMiddleware.run(sagaRoot)
