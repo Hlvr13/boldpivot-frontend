b3041b0ba0f810dbeb101d1da23d701ee34cffbe
LeviVazquez <hlvr.13@gmail.com>
2020-06-10 01:14:51 -0700

    :wrench:[Common-NewsCard] Added favorite func

--- src/components/Common/NewsCard.js
+++ src/components/Common/NewsCard.js
@@ -1,25 +1,49 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
+
+import { FiArrowDown, FiStar } from 'react-icons/fi'
+import { HeadlinesActions } from '../../redux/reducers/headlines'
+import { connect } from 'react-redux'
 
 const NewsCard = ({
-    key,
-    imgSrc = '',
-    title = 'No Tititle Available',
-    description = 'No Description.',
-    ...rest
+  customHeadlines,
+  index,
+  mainKey,
+  imgSrc = '',
+  title = 'No Tititle Available',
+  description = 'No Description.',
+  setToFavorite,
+  fav,
+  ...rest
 }) => {
+  console.log('fav', fav)
 
-    return (
-        <div className = 'news-card' key={key} {...rest}>
-            <img className = 'news-card-img' src={imgSrc} alt='top headline visual' />
-            <div className = 'news-card-content'>
-                <h4 className = 'news-card-content-title'>{title}</h4>
-                <p className = 'news-card-content-description'>{description}</p>
-            </div>
-            <div>
-                
-            </div>
-        </div>
-    )
+  const setToFav = () => {
+    setToFavorite(index)
+  }
+
+  return (
+    <div className='news-card' key={mainKey} {...rest}>
+      <img className='news-card-img' src={imgSrc} alt='top headline visual' />
+      <div className='news-card-content'>
+        <NavLink to={`/news-details/${index}`} className='news-card-content-title'>{title}</NavLink>
+        <p className='news-card-content-description'>{description}</p>
+      </div>
+      <div className='news-card-interactions'>
+        <FiArrowDown className='interaction-icon' />
+        <FiStar className={`interaction-icon ${fav ? 'fav' : ''}`} onClick={setToFav} />
+      </div>
+    </div>
+  )
 }
 
-export default NewsCard
+const mapStateToProps = state => ({
+})
+
+const mapDispatchToProps = dispatch => ({
+  // Headlines
+  setToFavorite: (index, id) => dispatch(HeadlinesActions.setToFavorite(index, id))
+
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(NewsCard)
