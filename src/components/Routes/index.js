782a58c1f150ae653675184a3ee6993ce731b02a
LeviVazquez <hlvr.13@gmail.com>
2020-06-10 01:16:38 -0700

    :wrench:[Routes-Index] Added news detail route path

--- src/components/Routes/index.js
+++ src/components/Routes/index.js
@@ -1,22 +1,23 @@
-import React, {useEffect} from 'react'
+import React from 'react'
 import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom'
 
 // Pages //
 import HeadlineOverview from '../Pages/Top_Headlines/Overview'
+import HeadlineDetails from '../Pages/Top_Headlines/Details'
 import NotFound from '../Pages/NotFound'
 
 const Routes = () => {
-    return (
-        <Router>
-            <Switch>
-                <Route exact path='/' component={HeadlineOverview} />
+  return (
+    <Router>
+      <Switch>
+        <Route exact path='/' component={HeadlineOverview} />
+        <Route exact path='/news-details/:id' component={HeadlineDetails} />
 
-
-                <Route exact path = '/not-found' component={NotFound}/>
-                <Redirect to = '/not-found'/>
-            </Switch>
-        </Router>
-    )
+        <Route exact path='/not-found' component={NotFound} />
+        <Redirect to='/not-found' />
+      </Switch>
+    </Router>
+  )
 }
 
 export default Routes
