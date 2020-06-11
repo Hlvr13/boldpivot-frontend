db8d50a0c47cc9112b65b53a160b762bb92c281c
LeviVazquez <hlvr.13@gmail.com>
2020-06-11 01:16:27 -0700

    :wrench:[NewsCard] removed favorite and added rating component

--- src/components/Common/NewsCard.js
+++ src/components/Common/NewsCard.js
@@ -1,37 +1,33 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
 
-import { FiArrowDown, FiStar } from 'react-icons/fi'
+import Rating from './Rating'
+import { FiCalendar } from 'react-icons/fi'
 import { HeadlinesActions } from '../../redux/reducers/headlines'
 import { connect } from 'react-redux'
 
 const NewsCard = ({
   customHeadlines,
-  index,
+  i,
   mainKey,
   imgSrc = '',
-  title = 'No Tititle Available',
-  description = 'No Description.',
+  title = 'No Title Available',
+  publishedAt,
   setToFavorite,
   fav,
+  review,
   ...rest
 }) => {
-  console.log('fav', fav)
-
-  const setToFav = () => {
-    setToFavorite(index)
-  }
 
   return (
     <div className='news-card' key={mainKey} {...rest}>
       <img className='news-card-img' src={imgSrc} alt='top headline visual' />
       <div className='news-card-content'>
-        <NavLink to={`/news-details/${index}`} className='news-card-content-title'>{title}</NavLink>
-        <p className='news-card-content-description'>{description}</p>
+        <NavLink to={`/news-details/${i}`} className='news-card-content-title' data-test = {`url-${i}`}>{title}</NavLink>
+        <p className='news-card-content-date'><FiCalendar /> {publishedAt}</p>
       </div>
       <div className='news-card-interactions'>
-        <FiArrowDown className='interaction-icon' />
-        <FiStar className={`interaction-icon ${fav ? 'fav' : ''}`} onClick={setToFav} />
+        <Rating defaulRate={review} i={i} dataTest = {`rating-${i}`}/>
       </div>
     </div>
   )
